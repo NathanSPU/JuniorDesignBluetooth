b414b9656defdb2eb1dd67deb14543858176baa6
NathanSPU <97091657+NathanSPU@users.noreply.github.com>
2023-05-12 10:25:18 -0700

    Eternal Damnation

--- Bluetooth.ino
+++ Bluetooth.ino
@@ -1,1 +1,19 @@
+#include <ArduinoBLE.h> // Not working with Arduino Mega, other option? Is it needed?
 
+#define CSC_Service_UUID "0x1816"
+#define CSC_MEASUREMENT_CHARACTERISTIC_UUID "0x2A5B"
+
+BLEDevice rpmSensor = BLE.available();
+int incomingByte;
+void setup() {
+  Serial.begin(9600);
+  pinMode(4, INPUT);
+  while (!Serial); // potential cut
+}
+
+void loop() {
+  if (rpmSensor) {
+    incomingByte = Serial.read();
+  }
+
+}
